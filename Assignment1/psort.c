a19cd8943aa58ff6ebc7871306889a0eb3460ed2
Alex Yegian <ayegian@nogbad.soe.ucsc.edu>
2022-01-15 07:18:26 -0800

    Done

--- Assignment1/psort.c
+++ Assignment1/psort.c
@@ -41,68 +41,34 @@ void singleProcessMergeSort(int arr[], int left, int right)
  */
 void multiProcessMergeSort(int arr[], int left, int right) 
 {
-//  printf("OPEN\n");
   FILE * f = fopen("psortmem","w+");
-//  printf("CLOSE\n");
   fclose(f);
-  // Delete this line, it's only here to fail the code quality chec
   int shid = shm_open("psortmem", O_RDWR|O_CREAT, 0);
   ftruncate(shid, (sizeof(int)*(1+(right-left)))/2);
-  printf("AFTER OPEN NO: %d ERROR: %d\n", shid, errno);
   int* shmem =(int *) mmap(0, (sizeof(int)*(1+(right-left))/2), PROT_READ|PROT_WRITE, MAP_SHARED,shid,0);
-  printf("BEF LOOP\n");
-//  for(int i = 0; i<rel_mid; ++i){
-//    shmem[i] = arr[i+left];
-//    printf("%d: %d\n",i, shmem[i]);
-//  }
   int rel_left = 0;
   int rel_mid = ((left+right)/2)-left+1;
   int rel_right = 1+(right-left);
   for(int i = 0; i<rel_mid; ++i){
     shmem[i] = arr[i+left];
-    printf("%d: %d\n",i, shmem[i]);
   }
-  printf("L: %d M: %d R: %d\n",rel_left, rel_mid, rel_right);
   int pid = fork();
   if(pid == 0){
-    printf("CHILD\n");
-    printf("AFTER OPEN NO: %d ERROR: %d\n", shid, errno);
-    printf("HERE\n");
-    for(int i = 0; i<rel_mid;++i){
-      printf("I: %d\n",i);
-      printf("BEF: %d\n",shmem[i]);
-      }
-    printf("HERE 2\n");
     singleProcessMergeSort(shmem, rel_left, rel_mid-1);
-    printf("SORTED\n");
-    for(int i = 0; i<rel_mid;++i){
-      printf("AFT: %d\n",shmem[i]);}
     munmap(shmem, (sizeof(int)*(1+(right-left)))/2);
     exit(0);
   }
   else{
-      singleProcessMergeSort(arr, left+rel_mid, left+rel_right-1);
-      wait(NULL);
-//    for(int i = left+rel_mid; i<left+rel_right;++i){
-//      printf("START ARR: %d\n",arr[i]);}
-//    singleProcessMergeSort(arr, left+rel_mid, left+rel_right-1);
-    for(int i = left+rel_mid; i<left+rel_right;++i){
-      printf("AFT ARR: %d\n",arr[i]);}
-
- //   printf("CHILD ENDED\n");
+    singleProcessMergeSort(arr, left+rel_mid, left+rel_right-1);
+    wait(NULL);
     for(int i = rel_left; i<rel_mid;++i){
       arr[i+left] = shmem[i];
     }
-//    for(int i = 0; i<rel_right;++i){
-//      printf("AFT 2: %d\n", arr[i+left]);}
     int mid = ((left+right)/2);
-//    printf("LEFT: %d MID: %d RIGHT: %d\n",left,mid,right);
     merge(arr, left, mid, right);
   }
-  printf("REMOVE\n");
   munmap(shmem, (sizeof(int)*(1+(right-left)))/2);
   shm_unlink("psortmem");
   close(shid);
   remove("psortmem");
-  // Your code goes here 
 }
--- Assignment1/usort.c
+++ Assignment1/usort.c
@@ -38,76 +38,35 @@ void singleProcessMergeSort(int arr[], int left, int right)
  * sub-array of ARR[] defined by the LEFT and RIGHT indexes.
  */
 void multiProcessMergeSort(int arr[], int left, int right){
- // Delete this line, it's only here to fail the code quality check
- // printf("START\n");
   FILE *f = fopen("usortmem","w+");
   fclose(f);
   key_t key = ftok("usortmem", 111);
   if(right-left <=0){
     return;
   }
-//  for(int i = left; i<=right;++i){
-//    printf("BEG ARR: %d\n", arr[i]);}
   int shid = shmget(key, (sizeof(int)*(1+(right-left)))/2, 0666|IPC_CREAT);
-  printf("ID: %d ERROR: %d\n", shid, errno);
   int * shmem= (int *)shmat(shid,(void *)0,0);
-//  printf("SIZE SHOULD BE: %ld LR: %d\n",sizeof(int)*(1+(right-left)), 1+(right-left));
-//  printf("%p\n", shmem);
-//  for(int i = 0; i<rel_mid;++i){
-//    printf("ARR I: %d\n", arr[left+i]);
-//    printf("I: %d\n",i);
-//    shmem[i]=arr[left+i];
-//  }
-  printf("AFTER THING\n");
   int rel_left = 0;
   int rel_mid = ((left+right)/2)-left+1;
   int rel_right = 1+(right-left);
   for(int i = 0; i<rel_mid;++i){
-//    printf("ARR I: %d\n", arr[left+i]);
-    printf("I: %d\n",i);
     shmem[i]=arr[left+i];
   }
 
-//  printf("L: %d M: %d R: %d\n",rel_left, rel_mid, rel_right);
   int pid = fork();
   if(pid == 0){
- //   key = ftok("shared", 211);
- //   shid = shmget(key, sizeof(int)*(1+(right-left)), IPC_CREAT);
- //   shmem = (int *) shmat(shid, (void *) NULL, 0);
-//    for(int i = 0; i<rel_mid;++i){
-//      printf("BEF: %d\n",shmem[i]);}
     singleProcessMergeSort(shmem, rel_left, rel_mid-1);
-    printf("SORTED\n");
-    for(int i = 0; i<rel_mid;++i){
-      printf("AFT: %d\n",shmem[i]);}
     shmdt(shmem);
     exit(0);
   }
   else{
     singleProcessMergeSort(arr, left+rel_mid, left+rel_right-1);
     wait(NULL);
-//    for(int i = left+rel_mid; i<left+rel_right;++i){
-//      printf("START ARR: %d\n",arr[i]);}
-//    singleProcessMergeSort(arr, left+rel_mid, left+rel_right-1);
-    for(int i = left+rel_mid; i<left+rel_right;++i){
-      printf("AFT ARR: %d\n",arr[i]);}
-
- //   printf("CHILD ENDED\n");
     for(int i = rel_left; i<rel_mid;++i){
       arr[i+left] = shmem[i];
     }
-//    for(int i = 0; i<rel_right;++i){
-//      printf("AFT 2: %d\n", arr[i+left]);}
     int mid = ((left+right)/2);
-//    printf("LEFT: %d MID: %d RIGHT: %d\n",left,mid,right);
     merge(arr, left, mid, right);
-//    for(int i = 0; i<(right-left);++i){
-//      printf("ELEMENT %d IS: %d\n", i, shmem[i]);
-//      arr[i+left] = shmem[i];
-//    }
-//    for(int i = left; i<=right; ++i){
-//      printf("ARR: %d\n", arr[i]);}
-//    printf("END\n");
     shmdt(shmem);
     shmctl(shid,IPC_RMID,NULL);
     remove("usortmem");
